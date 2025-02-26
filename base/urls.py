170811775882afcfb879311e22777145e3f69cbd
shihab173066 <shihab.hossainSH@gmail.com>
2025-02-27 02:31:53 +0600

    added delete function

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView, DetailView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
 ]
