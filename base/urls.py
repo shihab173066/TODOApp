0e16debbd140c2886dc60fb9252486a904c6f575
shihab173066 <shihab.hossainSH@gmail.com>
2025-02-27 00:07:15 +0600

    connected urls and views

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.tasklist, name='tasks')
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import request, response, HttpResponse, HttpRequest 
 
 # Create your views here.
+
+def tasklist(request):
+    return HttpResponse('<h1>To Do List</h1>')
