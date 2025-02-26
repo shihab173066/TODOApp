655c4e009f1880d84916542a68faa22614aa28ff
shihab173066 <shihab.hossainSH@gmail.com>
2025-02-27 01:03:12 +0600

    view of table updated

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TaskList
+from .views import TaskDetail, TaskList
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task')
 ]
