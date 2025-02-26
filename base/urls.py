2e82ad027be43b5349288156fd8e441a96ebe988
shihab173066 <shihab.hossainSH@gmail.com>
2025-02-27 01:29:45 +0600

    updated create

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TaskDetail, TaskList
+from .views import TaskDetail, TaskList, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task')
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
