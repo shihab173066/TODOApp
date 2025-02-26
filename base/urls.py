215dea52d9fc01a4a1eaa210ce8a3f201a6ae203
shihab173066 <shihab.hossainSH@gmail.com>
2025-02-27 00:44:22 +0600

    initial templates

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from .views import TaskList
 
 urlpatterns = [
-    path('', views.tasklist, name='tasks')
+    path('', TaskList.as_view(), name='tasks'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import request, response, HttpResponse, HttpRequest 
+from django.views.generic.list import ListView
+from .models import Task
 
 # Create your views here.
 
-def tasklist(request):
-    return HttpResponse('<h1>To Do List</h1>')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
