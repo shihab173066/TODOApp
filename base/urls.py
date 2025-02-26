66d9a1e35817a0389ac7168bf2299c3a839547f6
shihab173066 <shihab.hossainSH@gmail.com>
2025-02-27 01:49:54 +0600

    create and update view completed

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TaskDetail, TaskList, TaskCreate
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
 ]
